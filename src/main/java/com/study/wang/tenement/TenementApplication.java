cff7f7a4313751d32c21cebf0a8ddaa4c47aaf2e
wangziyang <1974576606@qq.com>
2020-03-03 03:02:54 +0800

    新增前端页面

--- src/main/java/com/study/wang/tenement/TenementApplication.java
+++ src/main/java/com/study/wang/tenement/TenementApplication.java
@@ -2,8 +2,9 @@ package com.study.wang.tenement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TenementApplication {
 
     public static void main(String[] args) {
