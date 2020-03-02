b5b255619b3273ae7d03dc6b5b7ea4a32eb5f5d5
wangziyang <1974576606@qq.com>
2020-03-03 02:48:05 +0800

    add file first

---
+++ src/main/java/com/study/wang/tenement/TenementApplication.java
@@ -0,0 +1,13 @@
+package com.study.wang.tenement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenementApplication.class, args);
+    }
+
+}
